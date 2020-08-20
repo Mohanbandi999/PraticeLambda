5e7f96239b90192a8a5b7e770de95d3c16f634b5
Mohan Bandi <mohanbandi999@gmail.com>
2020-08-20 19:43:35 +0530

    Updated hello world

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace PraticeLambda
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
         }
     }
 }
